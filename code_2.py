d3573c769d8b012156fabd547e5bc1c973a0e0d1
SofiSaynof <141125441+SofiSaynof@users.noreply.github.com>
2023-10-22 04:47:23 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,26 +1,38 @@
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+        }
+    public static void main(String[] args) {
 
+        int[] data = new int[10];
 
+public class RandomDataGenerator {
+    }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Random Number: " + item);
+        }
+        for (int item : data) {
 
-    return data
+    for item in data:
     main()
         print(f"Random Number: {item}")
-    data = generate_random_data()
-import random
 def main():
-    for item in data:
     data = generate_random_data()
-    for item in data:
-        print(f"Random Number: {item}")
 
-if __name__ == "__main__":
+import random
+    return data
 
 def generate_random_data():
-def main():
-    main()
-import random
-
     data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":def main():
+    main()
+    for item in data:
     return data
+if __name__ == "__main__":
+
+    data = generate_random_data()
+
+        print(f"Random Number: {item}")
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
